ee5c6bf6dd1ccfbe5c26949dc5e265146ca16606
Engin Turaman <engin.turaman@ecozum.com>
2021-03-08 08:52:44 +0300

    add project

---
+++ TestCodeUtility/TestCodeUtility/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestCodeUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
