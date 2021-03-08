a7848edde2027f8a53484c85cf104e57b678e692
Engin Turaman <engin.turaman@ecozum.com>
2021-03-08 10:31:52 +0300

    ..

---
+++ TestCodeUtility/TestCodeUtility/Models/VakifKatilimCurrencyStrings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestCodeUtility.Models
+{
+    public class VakifKatilimCurrencyStrings
+    {
+        public static readonly string TRY = "Türk Lirası";
+        public static readonly string EUR = "Euro";
+        public static readonly string USD = "Amerikan Doları";
+        public static readonly string GBP = "İngiliz Sterlini";
+        public static readonly string CHF = "İsviçre Frangı";
+        public static readonly string CAD = "Kanada Doları";
+        public static readonly string SEK = "İsveç Kronu";
+        public static readonly string JPY = "Japon Yeni";
+        public static readonly string RUB = "Rus Rublesi";
+        public static readonly string CNY = "Çin Yuanı";
+        public static readonly string RON = "Romen Leyi";
+        public static readonly string SAR = "Suudi Arabistan Riyali";
+        public static readonly string QAR = "Katar Riyali";
+        public static readonly string AUD = "Avustralya Doları";
+        public static readonly string NOK = "Norveç Kronu";
+        public static readonly string DKK = "Danimarka Kronu";
+        public static readonly string AED = "Birleşik Arap Emirlikleri Dirhemi";
+        public static readonly string XAU = "Altın";
+        public static readonly string XAG = "Gümüş";
+    }
+}
--- TestCodeUtility/TestCodeUtility/Program.cs
+++ TestCodeUtility/TestCodeUtility/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
+using TestCodeUtility.Models;
 
 namespace TestCodeUtility
 {
@@ -6,7 +9,33 @@ namespace TestCodeUtility
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           var code = ConvertCurrency<VakifKatilimCurrencyStrings>("Rus Rublesi");
+        }
+
+        //Belli bir class a göre 
+        public static string ConvertCurrency<T>(string currrencyCode) where T : new() 
+        {
+            try
+            {
+                //VakifKatilimCurrencyStrings sınıfının statik fieldlarının adı ve değerleri dictionary'e atılır.
+                var currencyDic = typeof(T)
+                    .GetFields(BindingFlags.Public | BindingFlags.Static)
+                    .Where(f => f.FieldType == typeof(string))
+                    .ToDictionary(f => f.Name, f => (string)f.GetValue(null));
+
+                var currency = currencyDic.FirstOrDefault(x => x.Value.Equals(currrencyCode));
+
+                if (!String.IsNullOrEmpty(currency.Key))
+                {
+                    return currency.Key;
+                }
+                else
+                    return currrencyCode;
+            }
+            catch
+            {
+                return currrencyCode;
+            }
         }
     }
 }
